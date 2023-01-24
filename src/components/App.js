efa4d8c512967482ed2e13172abcd07cc5be9163
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-01-24 19:03:21 +0530

    Task Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import NewComponent from "./NewComponent";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <NewComponent/>
+    </div>
   )
 }
 
